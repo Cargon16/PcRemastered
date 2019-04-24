af5420a4c3d1817c44b3fcd350ed54ee65ee19bf
Armingul <42656687+Armingul@users.noreply.github.com>
2019-04-24 20:17:21 +0200

    Modulo clientes cambiado y personal

--- PCRemasteredCodigo/src/Integracion/Personal/DAOPersonalImpl.java
+++ PCRemasteredCodigo/src/Integracion/Personal/DAOPersonalImpl.java
@@ -3,72 +3,127 @@
  */
 package Integracion.Personal;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author nacho710
- * @uml.annotations
- *     derived_abstraction="platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_tEtc4D6jEemri91jncE-mg"
- * @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_tEtc4D6jEemri91jncE-mg"
- */
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import Integracion.Connection.Connections;
+
 public class DAOPersonalImpl implements DAOPersonal {
-	/** 
-	* (non-Javadoc)
-	* @see DAOPersonal#create()
-	* @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_wSiDwD6jEemri91jncE-mg?INHERITED"
-	*/
-	public void create() {
-		// begin-user-code
-		// TODO Auto-generated method stub
 
-		// end-user-code
+	@Override
+	public Integer create(TPersonal personal) {
+
+		int id= 0;
+		int activo = 1;
+
+		if (personal.isActivo())
+			activo = 1;
+		else
+			activo = 0;
+
+		String insercion = "INSERT INTO personal " + "VALUES ('" + personal.getPass() +"', '"
+				+ personal.isActivo() + "', '" + personal.getSalario() + "', '" + personal.getID() + "', '" + personal.getTelefono() + "', '" +personal.getNombre() +"');";
+
+		Connection conn = Connections.getInstance();
+		if (conn != null) {
+			try {				
+				Statement stmt = conn.createStatement();
+				stmt.execute(insercion);		
+				if (!stmt.isClosed())
+					stmt.close();
+			} catch (SQLException e) {
+				id = -1;
+			}
+		}
+
+		return id;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see DAOPersonal#readByID()
-	* @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_wlCwED6jEemri91jncE-mg?INHERITED"
-	*/
-	public void readByID() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	@Override
+	public TPersonal readByID(int id) {
+		String lectura = "SELECT * FROM personal WHERE id=" + id + " FOR UPDATE;";
+		TPersonal retorno = null;
+		try {
+			Connection conn = Connections.getInstance();
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(lectura);
+				if (rs.next()) {
+					retorno = new TPersonal(rs.getString("pass"), rs.getBoolean("activo"), rs.getDouble("salario"),
+							rs.getInt("id"),rs.getInt("telefono"), rs.getString("nombre"));
+				}
+			}
+		} catch (SQLException e) {
+			retorno = null;
+		}
+		return retorno;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see DAOPersonal#readAll()
-	* @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_woiwAD6jEemri91jncE-mg?INHERITED"
-	*/
-	public void readAll() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	@Override
+	public ArrayList<TPersonal> readAll() {
+		
+		String lectura = "SELECT * FROM personal WHERE activo=1 FOR UPDATE;";
+		ArrayList<TPersonal> retorno = new ArrayList<TPersonal>();
+		try {
+			Connection conn = Connections.getInstance();
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(lectura);
+				while (rs.next()) {
+					TPersonal personal = new TPersonal(rs.getString("pass"), rs.getBoolean("activo"), rs.getDouble("salario"),
+							rs.getInt("id"),rs.getInt("telefono"), rs.getString("nombre"));
+					retorno.add(personal);
+				}
+			}
+		} catch (SQLException e) {
+			retorno = null;
+		}
+		return retorno;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see DAOPersonal#update()
-	* @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_wsKrwD6jEemri91jncE-mg?INHERITED"
-	*/
-	public void update() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+	@Override
+	public int update(TPersonal personal) {
+		int retorno = -1;
+		int activo = 0;
+		if(personal.isActivo())
+			activo = 1;
+		String actualizacion = "UPDATE personal SET  pass='" + personal.getPass() + "', activo='" + personal.isActivo()
+				+ "', salario='" + personal.getSalario() + "', id='" + personal.getID() + "', telefono='"
+				+ personal.getTelefono()+ "', nombre='" + personal.getNombre() +"' WHERE id=" + personal.getID();
+		try {
+			Connection conn = Connections.getInstance();
 
-		// end-user-code
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				stmt.executeUpdate(actualizacion);
+				retorno = personal.getID();
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			retorno = -1;
+		}
+
+		return retorno;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see DAOPersonal#delete()
-	* @generated "sourceid:platform:/resource/PCRemastered/Modelado%20de%20diseño.emx#_wxnzgD6jEemri91jncE-mg?INHERITED"
-	*/
-	public void delete() {
-		// begin-user-code
-		// TODO Auto-generated method stub
+	@Override
+	public int delete(int id) {
+		int retorno = -1;
 
-		// end-user-code
+		String borrado = "UPDATE personal SET activo=0 WHERE id=" + id;
+		try {
+			Connection conn =  Connections.getInstance();
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				stmt.executeUpdate(borrado);
+				retorno = id;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			retorno = -1;
+		}
+		return retorno;
 	}
 }
