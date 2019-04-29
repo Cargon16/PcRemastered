72725dd6799f65bc87a35347c32af61148961121
nacho710 <ignbaena@ucm.es>
2019-04-29 15:51:15 +0200

    commandos cliente

---
+++ PCRemasteredCodigo/src/Presentacion/Command/Cliente/CreateClienteCommand.java
@@ -0,0 +1,14 @@
+package Presentacion.Command.Cliente;
+
+import Presentacion.Command.Command;
+import Presentacion.Command.Contexto;
+
+public class CreateClienteCommand implements Command{
+
+	@Override
+	public void ejecutar(Contexto contexto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
